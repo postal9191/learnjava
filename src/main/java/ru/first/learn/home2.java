a940b42e94e653ede87148caf1e7471a8fd8eed6
postal9191 <postal.9191@mail.ru>
2022-10-17 00:09:38 +0300

    Задание №2

---
+++ src/main/java/ru/first/learn/home2.java
@@ -0,0 +1,17 @@
+package ru.first.learn;
+
+public class home2 {
+
+    public static void main(String[] args) {
+
+        Point p1 = new Point(-1, 6);
+        Point p2 = new Point(12, 33);
+
+        System.out.println("Расстояние с помощью функции = " + distance(p1, p2));
+        System.out.println("Расстояние с помощью метода из класса Point = " + p1.distanceInClass(p2));
+    }
+
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
+    }
+}
