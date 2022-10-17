fb76b353197317918ff15031f4336dccfc5f6bc1
postal9191 <postal.9191@mail.ru>
2022-10-17 09:33:36 +0300

    комит

--- src/main/java/ru/first/learn/home2.java
+++ src/main/java/ru/first/learn/home2.java
@@ -4,8 +4,8 @@ public class home2 {
 
     public static void main(String[] args) {
 
-        Point p1 = new Point(-1, 6);
-        Point p2 = new Point(12, 33);
+        Point p1 = new Point(1, 6);
+        Point p2 = new Point(10, 12);
 
         System.out.println("Расстояние с помощью функции = " + distance(p1, p2));
         System.out.println("Расстояние с помощью метода из класса Point = " + p1.distanceInClass(p2));
---
+++ src/test/java/ru/first/learn/SquareTests.java
@@ -0,0 +1,16 @@
+package ru.first.learn;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SquareTests {
+
+    @Test
+    public void testArea(){
+        Square s = new Square(5);
+
+        Assertions.assertEquals(s.area(),25);
+
+    }
+}
