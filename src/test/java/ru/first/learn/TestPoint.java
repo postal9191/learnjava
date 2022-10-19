1725908b92beff3b40c3767ecfc5af8a819165b0
postal9191 <postal.9191@mail.ru>
2022-10-19 14:27:09 +0300

    Задание №3 fix review + add gradle ver 6.9

--- src/test/java/ru/first/learn/TestPoint.java
+++ src/test/java/ru/first/learn/TestPoint.java
@@ -1,7 +1,7 @@
 package ru.first.learn;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.testng.Assert;
 
 public class TestPoint {
 
@@ -10,20 +10,30 @@ public class TestPoint {
         Point p1 = new Point(0, 1);
         Point p2 = new Point(0, 13);
 
-        Assertions.assertEquals(p1.distanceInClass(p2), 12, "Что-то не так");
+        Assert.assertEquals(p1.distanceInClass(p2), 12.0, "Что-то не так");
         System.out.println("Тест прошел успешно");
     }
 
     @Test
-    public void testArea1() {
+    public void longOfPointTwo() {
         Point p1 = new Point(1, 5);
         Point p2 = new Point(2, 12);
 
-        System.out.println("Заведомо провальный тест");
+        double proverka = home2.distance(p1, p2);
+        double formula = p1.distanceInClass(p2);
 
-        double o = Math.random() * 10;
+        Assert.assertEquals(formula, proverka, "Усп значения не совпали");
+        System.out.println("Формула = " + formula);
+        System.out.println("Проверочный ответ = " + proverka);
+    }
 
-        Assertions.assertEquals(Math.floor(p1.distanceInClass(p2)), Math.floor(o), "С округлением вниз значения не совпали");
-        Assertions.assertEquals(p1.distanceInClass(p2), o, "Усп значения не совпали");
+    @Test
+    public void longOfPointThree() {
+        Point p1 = new Point(-2, -3);
+        Point p2 = new Point(p1.x + 8, p1.y - 11);
+
+        double f = home2.distance(p1, p2);
+
+        Assert.assertTrue(f > 0);
     }
 }
